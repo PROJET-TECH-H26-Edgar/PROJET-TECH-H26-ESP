c33e05c34dfc085e2d186e69ae2a8b087ba7d36b
Edgar Gegout <2435196@cegeprdl.ca>
2026-02-27 10:33:12 -0500

    Mqtt OK

--- src/MqttConn.h
+++ src/MqttConn.h
@@ -4,30 +4,36 @@
 #include <WiFi.h>
 #include <WiFiClientSecure.h>
 #include <PubSubClient.h>
+#include "esp_wpa2.h"
+
+// Type de callback pour notifier le main Arduino (donné par l'ia)
+typedef void (*MessageCallback)(const char* topic, const char* message);
 
 class MqttConn {
-
-    public: 
-    MqttConn(const char* ssid, const char* mdp_wifi, const char* mqtt_serveur, int port, const char* mqtt_user, const char* mqtt_mdp, const char* ca_cert);
-    void begin();
-    void loop();
-    void publish(const char* topic, const char* message);
 
     private:
     void setupWifi();
     void reconnect();
-    static void callbackk(char* topic, byte* payload, unsigned int length);
+    static void callback(char* topic, byte* payload, unsigned int length);
     const char* _ssid;
-    const char* _wifi_password;
-    const char* _mqtt_server;
+    const char * _user_wifi;
+    const char* _mdp_wifi;
+    int _port;
+    const char* _mqtt_serveur;
     const char* _mqtt_user;
-    const char* _mqtt_password;
-    const char* _ca_cert;
+    const char* _mqtt_mdp;
     int _mqtt_port;
 
     WiFiClientSecure _secureClient;
     PubSubClient _client;
+    static MessageCallback _userCallback; // callback défini par le main (donné par l'ia)
 
+    public: 
+    MqttConn(const char* ssid,const char* user_wifi, const char* mdp_wifi, const char* mqtt_serveur, int port, const char* mqtt_user, const char* mqtt_mdp);
+    void begin();
+    void loop();
+    void publish(const char* topic, const char* message);
+    void onMessage(MessageCallback cb);
 };
 
 
